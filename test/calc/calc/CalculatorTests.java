6a35977ead38b247591ebed893f601703903a4c5
Kulkarni <PK@EUR.AD.SAG>
2017-12-13 14:56:19 +0530

    First commit

---
+++ test/calc/calc/CalculatorTests.java
@@ -0,0 +1,20 @@
+package calc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTests {
+
+	@Test
+	public void testAddition() {
+	
+		assertEquals("Addition output incorrect",5, CreateCalculator.add(2, 3));
+	}
+
+	@Test
+	public void testSubtraction() {
+	
+		assertEquals("Subtraction output incorrect",-1, CreateCalculator.subtract(2, 3));
+	}
+}
